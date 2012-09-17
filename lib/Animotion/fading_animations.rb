604d4103eaacace3bec1256cb67e6423109e6eaa
Silas J. Matson <silas@clearsightstudio.com>
2012-09-17 15:27:01 -0700

    cc

--- lib/Animotion/fading_animations.rb
+++ lib/Animotion/fading_animations.rb
@@ -1,7 +1,12 @@
 module Animotion
-  def fadeout(view, time=1)
+  def fadeout(view, time=1, opts={})
     animate time do
       view.alpha = 0
+    end
+    if opts[:remove] && opts[:remove] == true
+      run_after time do
+        view.removeFromSuperview  
+      end 
     end
   end
 
@@ -9,5 +14,16 @@ module Animotion
     animate time do
       view.alpha = 1
     end
+  end
+
+  # Taken from bubblewrap
+  # https://github.com/rubymotion/BubbleWrap
+  # https://github.com/rubymotion/BubbleWrap/blob/master/motion/core/app.rb lines 60-66 as of 9-17-12
+  def run_after(delay, &block)
+    NSTimer.scheduledTimerWithTimeInterval( delay,
+                                            target: block,
+                                            selector: "call:",
+                                            userInfo: nil,
+                                            repeats: false)
   end
 end
