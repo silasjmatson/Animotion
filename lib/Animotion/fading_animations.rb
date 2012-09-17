c29dba8d5e30afa10c6175d24c88776a7a4f6138
Silas J. Matson <silas@clearsightstudio.com>
2012-09-17 14:53:45 -0700

    Adding fadin and fadeout.

---
+++ lib/Animotion/fading_animations.rb
@@ -0,0 +1,13 @@
+module Animotion
+  def fadeout(view, time=1)
+    animate time do
+      view.alpha = 0
+    end
+  end
+
+  def fadein(view, time=1) do
+    animate time do
+      view.alpha = 1
+    end
+  end
+end
